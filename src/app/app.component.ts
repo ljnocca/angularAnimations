d988af22edb3b1b3d3b93ac1d7ef6de041b8c8c5
Laurent Nocca <lnocca@fanreact.com>
2017-12-17 11:53:49 -0600

    animations start

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+    ])
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item),1);
+  }
+
+}
