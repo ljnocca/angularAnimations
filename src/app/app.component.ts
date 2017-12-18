24f3eb53e6e8746b2e92a7f5f9d0bd55626070af
Laurent Nocca <lnocca@fanreact.com>
2017-12-17 19:18:10 -0600

    animations exercise finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -61,6 +61,48 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
           opacity: 0
         }))
       ])
+    ]),
+
+    trigger('list2', [
+      state('in', style({
+        'opacity': 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
     ])
   ]
 })
@@ -84,6 +126,14 @@ export class AppComponent {
 
   onDelete(item) {
     this.list.splice(this.list.indexOf(item),1);
+  }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
   }
 
 }
